6710c4272f3d95e86f09206d8aecf766e53eb229
Thomas Ikimi <themerc01@gmail.com>
2017-10-10 14:36:00 +0100

    note list new note in array

--- note-list-model.js
+++ note-list-model.js
@@ -1,13 +1,19 @@
 (function(exports){
   function NoteList(){
-    this.notes = [];
+    this.notes = ['note'];
   };
+
+  NoteList.prototype.newNote = function(text){
+    this.notes.push(new Note(text));
+  };
+  exports.newNote = this.newNote;
   exports.NoteList = NoteList;
+
 })(this);
 
-// (function(exports){
-//  function Note(text){
-//   this.text = text;
-// };
-//  exports.Note = Note;
-// })(this);
+(function(exports){
+ function Note(text){
+  this.text = text;
+};
+ exports.Note = Note;
+})(this);
--- testingJS.js
+++ testingJS.js
@@ -1,13 +1,21 @@
-// function testNoteIsCreated() {
-//   var note = new Note('hi');
-//   assert.isTrue(note.text === 'hi');
-// };
-//
-//  testNoteIsCreated();
+function testNoteIsCreated() {
+  var note = new Note('hi');
+  assert.isTrue(note.text === 'hi');
+};
+
+ testNoteIsCreated();
 
  function noteListCreated() {
    var noteList = new NoteList();
-   assert.isTrue(noteList.notes === []);
+   assert.isTrue(noteList.notes[0]=== 'note');
  };
 
  noteListCreated();
+
+ function testNewNoteInNotelist() {
+   var noteList = new NoteList();
+   noteList.newNote('hi');
+   assert.isTrue(noteList.notes[1].text === 'hi');
+ };
+
+ testNewNoteInNotelist();
