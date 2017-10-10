bff44b77ec0bbf6937d15f5a42ad58760da18228
Thomas Ikimi <themerc01@gmail.com>
2017-10-10 15:49:26 +0100

    return list objects

--- testingJS.js
+++ testingJS.js
@@ -19,3 +19,14 @@ function testNoteIsCreated() {
  };
 
  testNewNoteInNotelist();
+
+ function testNotesReturned() {
+   var noteList = new NoteList();
+   var notesBack = []
+   notesBack.push(noteList.newNote('hi'));
+   notesBack.push(noteList.newNote('hi'));
+
+   assert.isTrue(notesBack.length === 2);
+ };
+
+ testNotesReturned();
